922714de6800a62f7efe3444b610432ab276c99e
Gleb Osetrin <sir.gleban-gleb@yandex.ru>
2026-09-17 22:59:31 +0500

    removed self.deadline

--- task2.py
+++ task2.py
@@ -2,7 +2,6 @@ class Tester:
 
     def __init__(self, name, deadline=False):
         self.name = name
-        self.deadline = deadline
 
     def work_hard(self, deadline=True):
         if deadline:
